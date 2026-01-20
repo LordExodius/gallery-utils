389075c84e53b6ca0b2480c76b49368ca5f29610
LordExodius <oscar45697@gmail.com>
2026-01-20 01:35:53 -0500

    add logging and R2 upload compatibility

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # gallery-utils
 
-A collection of utilities for managing and displaying image galleries. Used for my blog and photo gallery.
+A collection of utilities for managing image galleries. Used for my blog and photo gallery.
 
 ## tools
-- `thumbnail.py`: A script to generate thumbnails for images in a specified directory.
-
+- `thumbnail.py`: A script to generate thumbnails for images. 
+    - This utility creates `.avif` thumbnails, uploads photos to Cloudflare R2, and metadata to Cloudflare D1 for gallery access. You can also run it in offline mode to just generate thumbnails without uploading.
+    - Any S3-compatible storage can be used by setting the appropriate environment variables in a `.env` file. See `thumbnail.py` for details.
+    - Cloudflare D1 is used to store metadata about the images and collections. Ensure you have the necessary database and tables set up before using the upload features.
 ## usage
 
-setup: 
+general setup: 
 ```
 git clone https://github.com/LordExodius/gallery-utils.git
 cd gallery-utils
@@ -15,10 +17,30 @@ pip install -r requirements.txt
 ```
 
 ### thumbnail.py 
-Usage: Run `python ./thumbnail.py [--help] [-w, --width WIDTH] [-q, --quality QUALITY] SOURCE_PATH`
 
-- `SOURCE_PATH`: Path to the directory containing images.
-- `-w, --width WIDTH`: Optional. Width of the generated thumbnails in pixels. Default is 1000px.
-- `-q, --quality QUALITY`: Optional. Quality of the generated thumbnails (1-100). Default is 75.
+#### Prerequisites:
+- In order to use any of the upload features, you need to have a Cloudflare R2 bucket (or other S3-compatible storage) and a Cloudflare D1 database set up.
+- You may ignore the upload features and just use the thumbnail generation locally with the `-oo` or `--offlineOnly` flag.
+- Set the following environment variables in a `.env` file in the project root:
+    - `AWS_ACCESS_KEY_ID`
+    - `AWS_SECRET_ACCESS_KEY`
+    - `AWS_ENDPOINT_URL`
+    - `S3_BUCKET_NAME`
+
+#### Usage:
+Run `python ./thumbnail.py SOURCE_PATH`
+
+#### Arguments:
+All arguments other than `SOURCE_PATH` are optional.
+| Parameter | Description |
+|-----------|-------------|
+| `SOURCE_PATH` | Path to the directory containing images OR path to a single image.
+| `-w, --width WIDTH` | Width of the generated thumbnails in pixels. Default is 1000px.
+| `-q, --quality QUALITY` | Quality of the generated thumbnails (1-100). Default is 75.
+| `-e, --effort EFFORT` | CPU effort spent improving compression (0: fastest, 9: slowest). Default is 4.
+| `-c, --collection COLLECTION` | Collection(s) to add photos to (use ; to delimit collections).
+| `-o, --overwrite` | Disable smart thumbnail generation and overwrite stored images. This will also cause all images to be re-uploaded.
+| `-oo, --offlineOnly` | Disable file uploads and only generate thumbnails locally.
+| `-uo, --uploadOnly` | Skip thumbnail generation and only upload to D1 and R2.
 
 Thumbnails are saved in a `thumbnails` subdirectory within the source directory.
--- thumbnail.py
+++ thumbnail.py
@@ -1,57 +1,113 @@
 # Auto generates thumbnails relative to a given directory.
 
+# stdlib imports
+from pathlib import Path
 import argparse
-import pathlib
-import time
-import pyvips as pv
+import atexit
+import json
+import logging.config
 import multiprocessing as mp
+import os
+
+# Third party 
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+import boto3
+import pyvips as pv
+
+logger = logging.getLogger("gallery_util")
+
+load_dotenv()
+
+def init_logging():
+    config_file = Path("logConfig.json")
+    with open(config_file) as cf:
+        config = json.load(cf)
+    logging.config.dictConfig(config)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
 
 def main():
+    init_logging()
+
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("source", help="Source directory or filename")
     parser.add_argument("-w", "--width", type=int, help="Width of thumbnail to generate (default: 1000px)")
-    parser.add_argument("-q", "--quality", type=int, help="Quality setting for thumbnail output (default: 75)")
+    parser.add_argument("-q", "--quality", type=int, help="Compression factor for thumbnail output (default: 75)")
     parser.add_argument("-e", "--effort", type=int, help="CPU effort spent improving compression (0: fastest, 9: slowest, 4: default)")
+    parser.add_argument("-c", "--collection", type=str, help="Collection(s) to add photos to (use ; to delimit collections)")
+    parser.add_argument("-o", "--overwrite", action="store_true", help="Disable smart thumbnail generation and overwrite stored images")
+    parser.add_argument("-oo", "--offlineOnly", action="store_true", help="Disable uploading and only generate thumbnails locally")
+    parser.add_argument("-uo", "--uploadOnly", action="store_true", help="Skip thumbnail generation and only upload to D1 and R2")
     args = parser.parse_args()
 
-    thumbWidth = 1000 if not args.width else int(args.width)
-    thumbQuality = 75 if not args.quality else int(args.quality)
-    thumbEffort = 4 if not args.effort else max(0, min(9, int(args.effort)))
+    # Define thumbnail export settings
+    width = 1000 if not args.width else max(64, int(args.width))
+    quality = 75 if not args.quality else max(0, min(100, int(args.quality)))
+    effort = 4 if not args.effort else max(0, min(9, int(args.effort)))
+    exportSettings = (width, quality, effort)
+    sourcePath = Path(args.source)
+    overwrite = bool(args.overwrite)
 
-    sourcePath = pathlib.Path(args.source)
-
-    # Generate map of source image to thumbnail path
+    # Generate path for all thumbnails
+    pathData = []
+    logger.info(f"Scanning path: {sourcePath}")
     if sourcePath.is_dir():
+        logger.info("Path provided is a DIRECTORY")
         sourceDir = sourcePath
         thumbDir = sourceDir / "thumbnails"
-        thumbnailGenerationData = [(
-            filePath, 
-            thumbDir / ".".join((filePath.name.rsplit(".")[0] + "-thumb", "avif")),
-            thumbWidth,
-            thumbQuality,
-            thumbEffort
-            ) for filePath in sourceDir.iterdir() if filePath.is_file()]
+        for filePath in sourceDir.iterdir():
+            if filePath.is_file():
+                thumbPath = thumbDir / ".".join((filePath.name.rsplit(".")[0] + "-thumb", "avif"))
+                # Do not regenerate thumbnail unless overwrite flag is enabled
+                if overwrite or not thumbPath.exists():
+                    pathData.append((filePath, thumbPath))
     else:
+        logger.info("Path provided is a FILE")
         sourceDir = sourcePath.parent
         thumbDir = sourceDir / "thumbnails"
-        thumbnailGenerationData = [(
-            sourcePath, 
-            thumbDir / ".".join((sourcePath.name.rsplit(".")[0] + "-thumb", "avif")),
-            thumbWidth,
-            thumbQuality,
-            thumbEffort
-            )]
-    print(f"found {len(thumbnailGenerationData)} files")
+        thumbPath = thumbDir / ".".join((sourcePath.name.rsplit(".")[0] + "-thumb", "avif"))
+        if overwrite or not thumbPath.exists():
+            pathData.append((sourcePath, thumbPath))
 
-    print(f"generating thumbnails...")
-    thumbDir.mkdir(parents=True, exist_ok=True)
-    startTime = time.time()
-    generate_thumbnails(thumbnailGenerationData)
-    endTime = time.time()
-    print(f"\ncompleted in {endTime - startTime} seconds")
+    logger.info(f"Found {len(pathData)} files")
 
-def generate_thumbnails(thumbnailGenerationData: list[tuple[pathlib.Path, pathlib.Path, int, int, int]]) -> None:
+    if not pathData:
+        logger.info("No new thumbnails found. Exiting early.")
+        return 0
+
+    if not args.uploadOnly:
+        thumbnailGenerationData = [(pd, exportSettings) for pd in pathData]
+        thumbDir.mkdir(parents=True, exist_ok=True)
+        generate_thumbnails(thumbnailGenerationData)
+    else:
+        logger.info("Thumbnail generation DISABLED")
+
+    if not args.offlineOnly:
+
+        # Call API to push photo names to DB; return array of id(s) associated with photos
+        logger.info("Updating thumbnail path tables...")
+        
+        # Upload photos and thumbnails to R2
+        logger.info("Connecting to object storage...")
+        aws_endpoint_url = os.getenv("AWS_ENDPOINT_URL")
+        s3 = boto3.client("s3", endpoint_url = aws_endpoint_url, region_name="auto")
+        for source, thumbnail in pathData:
+            push_to_r2(s3, source, thumbnail)
+
+        # Create linking for photos to collections (optional)
+        if args.collection:
+            collections = args.collection.split()
+    else:
+        logger.info("File uploads DISABLED")
+
+def generate_thumbnails(thumbnailGenerationData: list[tuple[tuple[Path, Path], tuple[int, int, int]]]) -> None:
+    """
+    Multiprocess thumbnail generation.
+    """
     numProcesses = mp.cpu_count()
     with mp.Pool(numProcesses) as pool:
         mp.freeze_support()
@@ -61,10 +117,25 @@ def generate_thumbnails(thumbnailGenerationData: list[tuple[pathlib.Path, pathli
             counter += 1
             print(f"processed {counter}/{numImages} images", end="\r")
         
-def generate_thumbnail(thumbGenData: tuple[pathlib.Path, pathlib.Path, int, int, int]) -> None:
-    sourcePath, targetPath, width, quality, effort = thumbGenData
-    thumb: pv.Image = pv.Image.thumbnail(sourcePath, width)
+def generate_thumbnail(thumbGenData: tuple[tuple[Path, Path], tuple[int, int, int]]) -> None:
+    """
+    Generates an `.avif` thumbnail with export settings applied.
+    """
+    pathData, exportSettings = thumbGenData
+    sourcePath, targetPath = pathData
+    width, quality, effort = exportSettings
+    thumb: pv.Image = pv.Image.thumbnail(sourcePath, width, size=pv.Size.DOWN)
     pv.Image.heifsave(thumb, targetPath, Q=quality, compression="av1", effort=effort)
+
+def push_to_r2(session, src: Path, thumbnail: Path):
+    """
+    Push source image and thumbnail to R2 via `boto3`
+    """
+    try:
+        session.upload_file(src, os.getenv("S3_BUCKET_NAME"), src.name)
+        session.upload_file(thumbnail, os.getenv("S3_BUCKET_NAME"), thumbnail.name)
+    except ClientError as e:
+        logger.error(e)
 
 if __name__ == "__main__":
     main()
